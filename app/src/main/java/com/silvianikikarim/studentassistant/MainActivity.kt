b97122a400c170ec1c7789de70f5d364453cf760
nhammond <nhammond@onitsmart.it>
2025-12-22 14:26:13 +0100

    Modifichestructure, eliminato OrarioFragment.kt & HomeFragment.kt (separation of concepts) (Usiamo Compose e non Fragment).

--- app/src/main/java/com/silvianikikarim/studentassistant/MainActivity.kt
+++ app/src/main/java/com/silvianikikarim/studentassistant/MainActivity.kt
@@ -3,7 +3,6 @@ package com.silvianikikarim.studentassistant
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material3.*
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.silvianikikarim.studentassistant.model.VotoDatabase
 import com.silvianikikarim.studentassistant.repository.VotoRepository
